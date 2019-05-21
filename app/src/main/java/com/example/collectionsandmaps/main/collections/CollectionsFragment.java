bb45038e0ec584112cf68cb950016b9e22c7acf2
Liubov Poshtovniuk <liuba.posh@gmail.com>
2019-05-21 16:46:47 +0300

    RxJava. Только для коллекций.

--- app/src/main/java/com/example/collectionsandmaps/main/collections/CollectionsFragment.java
+++ app/src/main/java/com/example/collectionsandmaps/main/collections/CollectionsFragment.java
@@ -25,7 +25,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-
+//==========================================
 public class CollectionsFragment extends DaggerFragment{
 
     @Inject
